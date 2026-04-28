59eeac888118cabaa68be3734af2b9f2b26af789
J-Nokwal <jagritnokwal9@gmail.com>
2026-04-28 19:30:46 +0530

    feat: update server URLs in Swagger configuration

--- src/core/config/swagger.ts
+++ src/core/config/swagger.ts
@@ -11,7 +11,12 @@ const options: swaggerJSDoc.Options = {
     servers: [
       {
         url: "http://localhost:5000",
+        description: "Local development server",
       },
+      {
+        url: "https://finance-dashboard.jagritnokwal.com",
+        description: "Production server",
+      }
     ],
     components: {
       securitySchemes: {
